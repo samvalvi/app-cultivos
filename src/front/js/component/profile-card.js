47e6f8d3a23c57f9cfb3d35fef3b4f6952664c84
samvalvi <samuelvalerin@gmail.com>
2021-04-22 22:17:20 +0000

    about fixed

--- src/front/js/component/profile-card.js
+++ src/front/js/component/profile-card.js
@@ -6,73 +6,75 @@ import "../../styles/profile-card.scss";
 export function Info() {
 	return (
 		<div className="p-3">
-			<div className="row justify-content-center">
-				<h2>Nosotros</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Ut enim ad minim veniam,
-				</p>
+			<div className="row text-center">
+				<div className="col">
+					<h2>Nosotros</h2>
+					<p className="mt-3">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam,
+					</p>
+				</div>
 			</div>
 			<div className="row">
-				<div className="col">
-					<div className="card shadow h-100">
+				<div className="col-md-6 col-lg-3">
+					<div className="mb-2 card shadow h-100">
 						<img src={user} className="mx-auto mt-3 card-img-top" alt="..." id="perfil" />
 						<div className="card-body">
 							<h5 className="text-center card-title">Ricardo</h5>
-							<p className="card-text">
+							<div className="card-text">
 								<ul>
 									<li>email: cubilloricardo@gmail.com</li>
 									<li>github: ricubi199</li>
 								</ul>
-							</p>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className="col">
-					<div className="card shadow h-100">
+				<div className="col-md-6 col-lg-3">
+					<div className="mb-2 card shadow h-100">
 						<img src={user} className="mx-auto mt-3 card-img-top" alt="..." id="perfil" />
 						<div className="card-body">
 							<h5 className="text-center card-title">Kevin</h5>
-							<p className="card-text">
+							<div className="card-text">
 								<ul>
 									<li>email: kev.rdgz@gmail.com</li>
 									<li>github: kevrdgz</li>
 								</ul>
-							</p>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className="col">
-					<div className="card shadow h-100">
+				<div className="col-md-6 col-lg-3">
+					<div className="mb-2 card shadow h-100">
 						<img src={user} className="mx-auto mt-3 card-img-top" alt="..." id="perfil" />
 						<div className="card-body">
 							<h5 className="text-center card-title">Ernesto</h5>
-							<p className="card-text">
+							<div className="card-text">
 								<ul>
 									<li>email: elopezc0308@gmail.com</li>
 									<li>github: Ernesto-03</li>
 								</ul>
-							</p>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div className="col">
-					<div className="card shadow h-100">
+				<div className="col-md-6 col-lg-3">
+					<div className="mb-2 card shadow h-100">
 						<img src={user} className="mx-auto mt-3 card-img-top" alt="..." id="perfil" />
 						<div className="card-body">
 							<h5 className="text-center card-title">Samuel</h5>
-							<p className="card-text">
+							<div className="card-text">
 								<ul>
 									<li>email: samuelvalerin@gmail.com</li>
 									<li>github: samvalvi</li>
 								</ul>
-							</p>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div className="row justify-content-center p-3">
-				<Link to="/" className="w-25 btn btn-dark" type="button">
+				<Link to="/" className="col-sm-2 btn btn-dark" type="button">
 					Regresar
 				</Link>
 			</div>
