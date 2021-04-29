11d13c1f6960334c9098600725214a6f3b11dff5
samvalvi <samuelvalerin@gmail.com>
2021-04-29 01:21:51 +0000

    bug form register fixed

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -41,120 +41,114 @@ export const Register = () => {
 	};
 
 	return (
-		<Container className="p-5 mt-5">
+		<Container className="p-3 mt-5">
 			<Row className="justify-content-center">
 				<Col sm={10} md={10}>
-					<Card className="shadow border-0 p-3">
-						<Col sm={12} lg={6}>
-							<div>
-								<h4 className="display-4">¡Bienvenido!</h4>
-								<h5 className="font-weight-normal">Cree su cuenta</h5>
+					<Card className="col-lg-7 border-0 mx-auto shadow p-3">
+						<h4 className="display-4">¡Bienvenido!</h4>
+						<h5 className="font-weight-normal">Cree su cuenta</h5>
+						{msg ? (
+							<div className="alert alert-danger" role="alert">
+								{msg}
 							</div>
+						) : null}
+						<Form onSubmit={() => handleSummit(event)}>
+							<Form.Row>
+								<Col lg={12}>
+									<Form.Group controlId="formGroupEmail">
+										<InputGroup className="mb-2 mr-sm-2">
+											<InputGroup.Prepend>
+												<InputGroup.Text>
+													<i className="fas fa-user" />
+												</InputGroup.Text>
+											</InputGroup.Prepend>
+											<Form.Control
+												type="text"
+												placeholder="primer nombre"
+												onChange={event => setFirstName(event.target.value)}
+												value={firstName}
+												required
+											/>
+										</InputGroup>
+									</Form.Group>
+								</Col>
+							</Form.Row>
 
-							<h3 className="display-5">Registro</h3>
-							{msg ? (
-								<div className="alert alert-danger" role="alert">
-									{msg}
-								</div>
-							) : null}
-							<Form onSubmit={() => handleSummit(event)}>
-								<Form.Row>
-									<Col lg={12}>
-										<Form.Group controlId="formGroupEmail">
-											<InputGroup className="mb-2 mr-sm-2">
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<i className="fas fa-user" />
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<Form.Control
-													type="text"
-													placeholder="primer nombre"
-													onChange={event => setFirstName(event.target.value)}
-													value={firstName}
-													required
-												/>
-											</InputGroup>
-										</Form.Group>
-									</Col>
-								</Form.Row>
+							<Form.Row>
+								<Col lg={12}>
+									<Form.Group controlId="formGroupEmail">
+										<InputGroup className="mb-2 mr-sm-2">
+											<InputGroup.Prepend>
+												<InputGroup.Text>
+													<i className="fas fa-user" />
+												</InputGroup.Text>
+											</InputGroup.Prepend>
+											<Form.Control
+												type="text"
+												placeholder="primer apellido"
+												onChange={event => setLastName(event.target.value)}
+												value={lastName}
+												required
+											/>
+										</InputGroup>
+									</Form.Group>
+								</Col>
+							</Form.Row>
 
-								<Form.Row>
-									<Col lg={12}>
-										<Form.Group controlId="formGroupEmail">
-											<InputGroup className="mb-2 mr-sm-2">
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<i className="fas fa-user" />
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<Form.Control
-													type="text"
-													placeholder="primer apellido"
-													onChange={event => setLastName(event.target.value)}
-													value={lastName}
-													required
-												/>
-											</InputGroup>
-										</Form.Group>
-									</Col>
-								</Form.Row>
+							<Form.Row>
+								<Col lg={12}>
+									<Form.Group controlId="formGroupEmail">
+										<InputGroup className="mb-2 mr-sm-2">
+											<InputGroup.Prepend>
+												<InputGroup.Text>
+													<i className="fas fa-at" />
+												</InputGroup.Text>
+											</InputGroup.Prepend>
+											<Form.Control
+												type="email"
+												placeholder="correo electrónico"
+												onChange={event => setEmail(event.target.value)}
+												value={email}
+												required
+											/>
+										</InputGroup>
+									</Form.Group>
+								</Col>
+							</Form.Row>
 
-								<Form.Row>
-									<Col lg={12}>
-										<Form.Group controlId="formGroupEmail">
-											<InputGroup className="mb-2 mr-sm-2">
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<i className="fas fa-at" />
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<Form.Control
-													type="email"
-													placeholder="correo electrónico"
-													onChange={event => setEmail(event.target.value)}
-													value={email}
-													required
-												/>
-											</InputGroup>
-										</Form.Group>
-									</Col>
-								</Form.Row>
+							<Form.Row>
+								<Col lg={12}>
+									<Form.Group controlId="formGroupPassword">
+										<InputGroup className="mb-2 mr-sm-2">
+											<InputGroup.Prepend>
+												<InputGroup.Text>
+													<i className="fas fa-lock" />
+												</InputGroup.Text>
+											</InputGroup.Prepend>
+											<Form.Control
+												type="password"
+												placeholder="contraseña"
+												onChange={event => setPassword(event.target.value)}
+												value={password}
+												required
+											/>
+										</InputGroup>
+									</Form.Group>
+								</Col>
+							</Form.Row>
 
-								<Form.Row>
-									<Col lg={12}>
-										<Form.Group controlId="formGroupPassword">
-											<InputGroup className="mb-2 mr-sm-2">
-												<InputGroup.Prepend>
-													<InputGroup.Text>
-														<i className="fas fa-lock" />
-													</InputGroup.Text>
-												</InputGroup.Prepend>
-												<Form.Control
-													type="password"
-													placeholder="contraseña"
-													onChange={event => setPassword(event.target.value)}
-													value={password}
-													required
-												/>
-											</InputGroup>
-										</Form.Group>
-									</Col>
-								</Form.Row>
-
-								<Form.Row>
-									<Col lg={10}>
-										<Button variant="primary" type="submit" className="btn btn-dark">
-											Crear
-										</Button>{" "}
-										<Link to="/" role="button" className="ml-1 btn btn-light" variant="light">
-											Cancelar
-										</Link>
-									</Col>
-								</Form.Row>
-							</Form>
-							{auth ? <Redirect to="/login" /> : null}
-						</Col>
+							<Form.Row>
+								<Col lg={10}>
+									<Button variant="primary" type="submit" className="btn btn-dark">
+										Crear
+									</Button>{" "}
+									<Link to="/" role="button" className="ml-1 btn btn-light" variant="light">
+										Cancelar
+									</Link>
+								</Col>
+							</Form.Row>
+						</Form>
+						{auth ? <Redirect to="/login" /> : null}
 					</Card>
 				</Col>
 			</Row>
